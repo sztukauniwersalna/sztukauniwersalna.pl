82ad086c978b737789da69343165f0943a3e80a7
Maciej Chałapuk <maciej@chalapuk.pl>
2017-05-14 17:49:56 +0200

    + rendered top links

--- _layouts/default.tsx
+++ _layouts/default.tsx
@@ -1,14 +1,24 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 
 interface Props {
   title : string;
   body : string;
+  topLinks : any;
 };
 
-export default ({ title, body } : Props) => (
-  <main>
-    <h1>{ title }</h1>
-    <div dangerouslySetInnerHTML={ { __html: body } } />
-  </main>
+export default ({ title, body, topLinks } : Props) => (
+  <div>
+    <nav>
+      <ul>
+      { Object.keys(topLinks)
+          .map((name, key) => <Link to={ topLinks[name] } key={ key }>{ name }</Link>) }
+      </ul>
+    </nav>
+    <header>
+      <h1>{ title }</h1>
+      <div dangerouslySetInnerHTML={ { __html: body } } />
+    </header>
+  </div>
 );
 
