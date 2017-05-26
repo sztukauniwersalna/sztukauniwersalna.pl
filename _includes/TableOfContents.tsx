e0ca2570147ed44df4e510a78e2bf560ae33da4f
Maciej Chałapuk <maciej@chalapuk.pl>
2017-05-27 01:54:06 +0200

    ~ h2 instead of h1 used in generated sitemap

--- _includes/TableOfContents.tsx
+++ _includes/TableOfContents.tsx
@@ -17,9 +17,9 @@ export default ({ categories } : Props) => (
     .map((key : string) => categories[key])
     .map(({ url, title, pages } : Category, key: number) => (
     <div key={ key }>
-      <h1>
+      <h2>
         <Link to={ url }>{ title }</Link>
-      </h1>
+      </h2>
       <ul>
       { pages.map(({ title, url } : Page, key: number) => (
         <li key={ key }><Link to={ url }>{ title }</Link></li>
