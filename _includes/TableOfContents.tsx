1035143c01f90e20c52148814abbfb3d49970c4d
Maciej Chałapuk <maciej@chalapuk.pl>
2017-06-10 22:50:58 +0200

    ~ sitemap has one root, renders categories only once and renders uncategorized at root level

--- _includes/TableOfContents.tsx
+++ _includes/TableOfContents.tsx
@@ -8,34 +8,32 @@ interface Props {
 };
 
 export default ({ website } : Props) => (
-  <div>
-  { Object.keys(website.categories)
-    .map((key : string) => website.categories[key])
-    .map(({ url, title, pages } : Category, key: number) => (
-    <div key={ key }>
-      <h2>
-        <Link to={ url }>{ title }</Link>
-      </h2>
+  <ul>
+    <li>
+      <Link to='/'>{ website.getPageOfUrl('/').title }</Link>
       <ul>
-      { pages.map(({ title, url } : Page, key: number) => (
-        <li key={ key }><Link to={ url }>{ title }</Link></li>
+      { Object.keys(website.categories)
+        .map((key : string) => website.categories[key])
+        .map(({ url, title, pages } : Category, key: number) => (
+        <li key={ key }>
+          <Link to={ url }>{ title }</Link>
+          <ul>
+          { pages.map(({ title, url } : Page, key: number) => (
+            <li key={ key }><Link to={ url }>{ title }</Link></li>
+          )) }
+          </ul>
+        </li>
       )) }
-      </ul>
-    </div>
-  )) }
-    <div key={ 'uncategorized' }>
-      <h2>
-        { 'Nieskategoryzowane' }
-      </h2>
-      <ul>
       { Object.keys(website.pages)
-        .map((key : string) => website.pages[key])
-        .filter((page : Page) => page.categories.length == 0)
-        .map(({ title, url } : Page, key: number) => (
-         <li key={ key }><Link to={ url }>{ title }</Link></li>
-        )) }
+          .map((key : string) => website.pages[key])
+          .filter((page : Page) => page.categories.length == 0)
+          .filter((page : Page) => !(page instanceof Category))
+          .filter((page : Page) => ['/', '/404'].indexOf(page.url) === -1)
+          .map(({ title, url } : Page, key: number) => (
+            <li key={ key }><Link to={ url }>{ title }</Link></li>
+          )) }
       </ul>
-    </div>
-  </div>
+    </li>
+  </ul>
 );
 
