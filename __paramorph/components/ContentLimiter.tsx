e69634b5a5acfa85a199b43a8ea7bae5267c556b
Maciej Chałapuk <maciej@chalapuk.pl>
2017-07-02 04:25:38 +0200

    ~ limiting should be working

--- __paramorph/components/ContentLimiter.tsx
+++ __paramorph/components/ContentLimiter.tsx
@@ -7,43 +7,66 @@ interface Props {
 }
 
 export const ContentLimiter = ({ children, limit, ...props } : Props) => {
-  const childs = ([] as ReactElement<any>[]).concat((children || []) as ReactElement<any>[]);
-  if (!limit || childs.length === 0) {
+  if (!limit) {
     return <div>{ children }</div>;
   }
 
-  const output = [] as ReactElement<any>[];
-  let characters = limit;
-
-  for (const child of childs) {
-    if (characters === 0) {
-      break;
-    }
-    switch (child.type) {
-      case 'p':
-        characters = renderParagraph(child, characters, output);
-        break;
-      case 'ul':
-      case 'ol':
-        characters = renderList(child, characters, output);
-        break;
-      default:
-        output.push(cloneElement(child, { ...props, limit: characters }));
-        characters = 0;
-        break;
-    }
-  }
+  const output = [] as ReactNode[];
+  limitContent(children, limit, props, output);
   return <div>{ output }</div>;
 }
 
-function renderParagraph(child : ReactElement<any>, limit : number, output : ReactElement<any>[]) {
-  output.push(<p>Paragraph limited to { limit }.</p>);
-  return 0;
+function limitContent(
+  children : ReactNode | undefined,
+  limit : number,
+  props : any,
+  output : ReactNode[]
+) {
+  switch (typeof children) {
+    case 'undefined':
+      return limit;
+    case 'number':
+      return limitString(Number(children as number).toString(), limit, output);
+    case 'string':
+      return limitString(children as string, limit, output);
+    default:
+      return limitReactElement(children, limit, props, output);
+  }
 }
 
-function renderList(child : ReactElement<any>, limit : number, output : ReactElement<any>[]) {
-  output.push(<p>Paragraph limited to { limit }.</p>)
-  return 0;
+function limitString(child : string, limit : number, output : ReactNode[]) {
+  output.push(child.substring(0, limit));
+  return Math.max(0, limit - child.length);
+}
+
+function limitReactElement(
+  children : ReactNode | undefined,
+  limit : number,
+  props : any,
+  output : ReactNode[]
+) {
+  let characters = limit;
+
+  for (const child of asReactElementArray(children)) {
+    if (characters === 0) {
+      break;
+    }
+    const newChildren = [] as ReactNode[];
+    characters = limitContent(child.props.children, characters, props, newChildren)
+    output.push(cloneElement(child, props, newChildren));
+  }
+
+  return characters;
+}
+
+function asReactElementArray(children ?: ReactNode) {
+  if (typeof children === 'undefined') {
+    return [] as ReactElement<any>[];
+  }
+  if (typeof children !== 'object') {
+    throw new Error(`unexpected value: ${children}`);
+  }
+  return ([] as ReactElement<any>[]).concat(children as ReactElement<any>[]);
 }
 
 export default ContentLimiter;
--- _posts/test.markdown
+++ _posts/test.markdown
@@ -3,6 +3,7 @@ title: Test
 category: 'Sztuka Gotowania'
 role: category
 tags: [ hello, tags ]
+limit: 4
 
 ---
 Sharks with frickin' lasers attached to their heads.
