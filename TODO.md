c6c8b84390828eca840335a06fafca7aeaf3bf4f
Maciej Chałapuk <maciej@chalapuk.pl>
2017-07-11 23:02:49 +0200

    + Feed rendered as ToC when in already in Feed

--- TODO.md
+++ TODO.md
@@ -16,7 +16,7 @@
 1. ~Tags rendered as meta keywords~,
 1. ~Meta description rendered from first paragraph~,
 1. ~Only first paragraph rendered in feed~,
-1. Feeds rendered as ToC when already in feed,
+1. ~Feeds rendered as ToC when already in feed~,
 1. ToC rendered partially when in feed,
 1. Generated XML sitemap,
 1. Layout coded in HTML/CSS,
--- _includes/Feed.tsx
+++ _includes/Feed.tsx
@@ -5,14 +5,20 @@ import { Page, Website } from 'paramorph/models';
 
 import CategoryList from './CategoryList';
 import TagList from './TagList';
+import { Branch as TocBranch } from './TableOfContents';
 
 export interface Props {
   website : Website;
   page : Page;
   feed : Page[];
+  respectLimit ?: boolean;
 };
 
-export function Feed({ website, page, feed } : Props) {
+export function Feed({ website, page, feed, respectLimit = false, ...props } : Props) {
+  if (respectLimit) {
+    return <TocBranch pages={ feed } shallow { ...props } />;
+  }
+
   return (
     <div>
   { feed.map((page : Page, key : number) => {
