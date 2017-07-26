b1e45560211aafd0522021dba10e0467068fc348
Maciej Chałapuk <maciej@chalapuk.pl>
2017-07-27 01:59:56 +0200

    + used TopBar component in ParrotLayout

--- _layouts/ParrotLayout.tsx
+++ _layouts/ParrotLayout.tsx
@@ -6,10 +6,11 @@ import withStyles from 'isomorphic-style-loader/lib/withStyles';
 
 import Crumbs from '../_includes/Crumbs';
 import TagList from '../_includes/TagList';
+import TopBar from 'parrot-layout/TopBar';
 
 const s = require('parrot-layout/root.scss');
 
-interface Props {
+export interface Props {
   website : Website;
   page : Page;
 }
@@ -24,15 +25,9 @@ export function ParrotLayout({ website, page } : Props) {
         href="https://fonts.googleapis.com/css?family=Andada|Roboto+Slab:300,400,700"
         rel="stylesheet"
       />
-      <header>
-        <nav>
-          <ul>
-          { website.menu.map((entry : MenuEntry, key : number) => (
-            <li key={ key }><Link to={ entry.url }>{ entry.short }</Link></li>
-          )) }
-          </ul>
-        </nav>
-      </header>
+      <div className={ s.header }>
+        <TopBar website={ website } page={ page } />
+      </div>
       <main>
         <h1><Link to={ page.url }>{ page.title }</Link></h1>
         <Crumbs website={ website } page={ page } />
